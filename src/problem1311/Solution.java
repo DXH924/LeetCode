07d5d329b58b602625d0ef7da08fc6a920c75219
DXH924 <chhizhang@163.com>
2021-03-06 12:05:08 +0800

    2021/03/06

---
+++ src/problem1311/Solution.java
@@ -0,0 +1,18 @@
+package problem1311;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        return null;
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        int[] arr = new int[] {4,23,-6,-15,-24,-17,6,-47,20,39,-26,9,-44,39,-50};
+        System.out.println(Arrays.toString(new Solution().arrayRankTransform(arr)));
+    }
+}
